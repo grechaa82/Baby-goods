3c1acd0d0f50ea9b55652fb4b52d03cf32803de6
Grechaa8.2 <danilgusejnov@gmail.com>
2023-04-28 11:48:30 +0300

    Добавил обновление данных у пользователя

--- Baby-goods.Common/Interfaces/IUserService.cs
+++ Baby-goods.Common/Interfaces/IUserService.cs
@@ -4,5 +4,10 @@
     {
         Task<User> GetByEmail(string email);
         Task Create(User user);
+        Task<User> GetById(Guid id);
+        Task UpdateUsername(Guid id, string username);
+        Task UpdatePassword(Guid id, string password);
+        Task UpdateFirstNameAndLastName(Guid id, string firstName, string lastName);
+        Task UpdatePhone(Guid id, string phone);
     }
 }
--- Baby-goods/Controllers/UserController.cs
+++ Baby-goods/Controllers/UserController.cs
@@ -1,11 +1,13 @@
 ﻿using Baby_goods.Common.Interfaces;
 using Baby_goods.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Baby_goods.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class UserController : Controller
     {
         private readonly IUserService _userService;
@@ -15,6 +17,15 @@ namespace Baby_goods.Controllers
             _userService = userService;
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            var user = await _userService.GetById(id);
+
+             return Ok(user);
+        }
+
+        [AllowAnonymous]
         [HttpPost("registration")]
         public async Task<IActionResult> Registration(UserRegistrationRequest request)
         {
@@ -40,6 +51,66 @@ namespace Baby_goods.Controllers
             await _userService.Create(newUser);
 
             return Ok();
+        }
+
+        [HttpPut("{id}/password")]
+        public async Task<IActionResult> UpdatePassword(Guid id, string password)
+        {
+            try
+            {
+                await _userService.UpdatePassword(id, password);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}/username")]
+        public async Task<IActionResult> UpdateUsername(Guid id, string newUsername)
+        {
+            try
+            {
+                await _userService.UpdateUsername(id, newUsername);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}/firstNameAndLastName")]
+        public async Task<IActionResult> UpdateFirstNameAndLastName(Guid id, string firstName, string lastName)
+        {
+            try
+            {
+                await _userService.UpdateFirstNameAndLastName(id, firstName, lastName);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}/phone")]
+        public async Task<IActionResult> UpdatePhone(Guid id, string phone)
+        {
+            try
+            {
+                await _userService.UpdatePhone(id, phone);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
